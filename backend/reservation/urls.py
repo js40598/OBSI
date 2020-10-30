5c0328d3c09a2ca54b05168d9539d9976477bda2
unknown <szymonjarzab1997@gmail.com>
2020-10-30 20:30:08 +0100

    Calendar pagination

--- backend/reservation/urls.py
+++ backend/reservation/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from reservation import views
 
 urlpatterns = [
-    path('<slug:slug>', views.reservation_year, name='reservation_year'),
+    path('', views.reservation_year, name='reservation_year'),
 ]
 
--- backend/reservation/views.py
+++ backend/reservation/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from calendar import Calendar
 from datetime import datetime
 
 
 @login_required
-def reservation_year(request, year_slug=datetime.now().year):
-
+def reservation_year(request):
+    current_year = datetime.now().year
+    paginator_years = [current_year-1, current_year, current_year+1]
+    paginator = Paginator(paginator_years, 1)
+    year = request.GET.get('year')
+    if year is None:
+        year = current_year
+    paged_year = paginator.get_page(year)
     context = {
-        'dates': Calendar()
+        'dates': Calendar(),
+        'current_year': current_year,
+        'paged_year': paged_year,
+        'paginator_years': paginator_years,
+        'year': int(year),
     }
     return render(request, 'reservation/year.html', context)
