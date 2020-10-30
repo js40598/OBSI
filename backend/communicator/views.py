ed8628ee0794d926635b65c8b1ff636a86c83e4f
unknown <szymonjarzab1997@gmail.com>
2020-10-30 23:56:07 +0100

    reservation communicator instances

--- backend/communicator/views.py
+++ backend/communicator/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from datetime import datetime
+from django.core.exceptions import ObjectDoesNotExist
+from reservation.models import Reservation
 
 # Create your views here.
+
+
+def communicator(request, year, month, day, time):
+    try:
+        reservation = Reservation.objects.get(year_slug=year,
+                                              month_slug=month,
+                                              day_slug=day,
+                                              time=time)
+    except ObjectDoesNotExist:
+        return redirect('reservation_day', year, month, day)
+    context = {
+        'current_year': datetime.now().year,
+    }
+    return render(request, 'communicator/communicator.html', context)
--- backend/reservation/urls.py
+++ backend/reservation/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from reservation import views
 
 urlpatterns = [
     path('<int:curr_year>', views.reservation_year, name='reservation_year'),
     path('<int:year>/<int:month>/<int:day>', views.reservation_day, name='reservation_day'),
+    path('<int:year>/<int:month>/<int:day>/<int:time>/communicator', include('communicator.urls')),
 ]
 
