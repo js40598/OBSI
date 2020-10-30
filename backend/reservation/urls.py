896d5d677b703ab13cff98c9f97d0fa318e1d125
unknown <szymonjarzab1997@gmail.com>
2020-10-30 22:05:31 +0100

    reservation year view

--- backend/reservation/urls.py
+++ backend/reservation/urls.py
@@ -3,5 +3,6 @@ from reservation import views
 
 urlpatterns = [
     path('', views.reservation_year, name='reservation_year'),
+    path('<int:year>/<int:month>/<int:day>', views.reservation_day, name='reservation_day'),
 ]
 
--- backend/reservation/views.py
+++ backend/reservation/views.py
@@ -7,6 +7,7 @@ from datetime import datetime
 
 @login_required
 def reservation_year(request):
+    # Pagination
     current_year = datetime.now().year
     paginator_years = [current_year-1, current_year, current_year+1]
     paginator = Paginator(paginator_years, 1)
@@ -14,11 +15,21 @@ def reservation_year(request):
     if year is None:
         year = current_year
     paged_year = paginator.get_page(year)
+
+    # Display
+    calendar = [[Calendar().itermonthdays4(current_year, i), i] for i in range(1, 13)]
     context = {
         'dates': Calendar(),
         'current_year': current_year,
         'paged_year': paged_year,
         'paginator_years': paginator_years,
         'year': int(year),
+        'calendar': calendar,
     }
+
     return render(request, 'reservation/year.html', context)
+
+
+@login_required
+def reservation_day(request, year, month, day):
+    return render(request, 'reservation/day.html')
