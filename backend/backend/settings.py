50020b4b3882c28d83cb02b436a7cd185c34043f
unknown <szymonjarzab1997@gmail.com>
2020-11-04 13:48:56 +0100

    heroku config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '2u1csc_b*$kwa1yqtf1&a^=noqsu)tlnizm4=tug#l*97ay7%!'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '2u1csc_b*$kwa1yqtf1&a^=noqsu)tlnizm4=tug#l*97ay7%!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
