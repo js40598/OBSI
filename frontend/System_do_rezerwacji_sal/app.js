9a115ea91543dd5ae596f17ff74a8b23de29cb45
GrzegorzP96 <73230029+GrzegorzP96@users.noreply.github.com>
2021-01-19 23:56:45 +0100

    update

--- frontend/System_do_rezerwacji_sal/app.js
+++ frontend/System_do_rezerwacji_sal/app.js
@@ -3,4 +3,29 @@ function openform(){
 }
 function closeform(){
     document.getElementById("myform").style.display="none";
+};
+
+function isLeapYear(y) {
+    return ((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0);
+}
+
+$(document).ready(function() {
+    var today = new Date();
+    var first = new Date();
+    first.setDate(1);
+
+    // Today
+    $(".days span").eq(today.getDate() - 1).addClass("today");
+
+    // First day of month
+    var bodyClass = "day" + first.getDay();
+
+    // Days in month
+    bodyClass += " month" + first.getMonth();
+
+    // Leap year
+    bodyClass += isLeapYear(today.getFullYear()) ? " leap" : "";
+
+    $("body").addClass(bodyClass);
+
 });
